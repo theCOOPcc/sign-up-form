845444617bbf0089aba8c391a25ae4a20f4ab238
Cory <corys4139@gmail.com>
2021-04-22 12:44:30 -0400

    update form model in django

--- backend/form/admin.py
+++ backend/form/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Form
 
 class FormAdmin(admin.ModelAdmin):
-  list_display = ('skills', 'techs', 'tell_us')
+  list_display = ('first_name', 'last_name', 'email', 'pronouns', 'why_join', 'linkedin', 'github', 'portfolio', 'engineer_skillset', 'engineer_techs', 'design_skillset', 'design_techs', 'data_sci_skillset', 'bootcamps', 'help_with', 'avail_dates')
 
 # Register your models here.
 
--- backend/form/models.py
+++ backend/form/models.py
@@ -5,9 +5,23 @@ from django.db import models
 # TODO: update form model to reflect selector choices
 
 class Form(models.Model):
-  skills = models.CharField(max_length=50)
-  techs = models.CharField(max_length=50)
-  tell_us = models.TextField()
+  role = models.CharField(max_length=50, default='', blank=True)
+  first_name = models.CharField(max_length=50, default='', blank=True)
+  last_name = models.CharField(max_length=50, default='', blank=True)
+  email = models.EmailField(max_length=254, default='', blank=True)
+  linkedin = models.CharField(max_length=254, default='', blank=True)
+  github = models.CharField(max_length=254, default='', blank=True)
+  portfolio = models.CharField(max_length=254,default='', blank=True)
+  engineer_skillset = models.CharField(max_length=50, default='', blank=True)
+  engineer_techs = models.CharField(max_length=50, default='', blank=True)
+  why_join = models.TextField(default='')
+  design_skillset = models.CharField(max_length=50, default='', blank=True)
+  design_techs = models.CharField(max_length=50, default='', blank=True)
+  data_sci_skillset = models.CharField(max_length=50, default='', blank=True)
+  bootcamps = models.CharField(max_length=50, default='', blank=True)
+  help_with = models.CharField(max_length=50, default='', blank=True)
+  avail_dates = models.CharField(max_length=50, default='', blank=True)
+  pronouns = models.CharField(max_length=50, default='', blank=True)
 
   def _str_(self):
-    return self.tell_us
+    return self.last_name
