2c1d423f1e4a30494ff103cb71b7956e13412308
Cory <corys4139@gmail.com>
2021-05-07 10:54:51 -0400

    redeploy db

--- backend/settings.py
+++ backend/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from dotenv import load_dotenv
 import dotenv
-import os
+import os 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
